fa56fa52e9f1535cddb4d5d5730d137a4bd56ee4
Iver Småge men bærbar <iversmage@gmail.com>
2022-07-08 17:05:42 +0200

    do vercel

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
     postcss: true
   }),
   kit: {
-    adapter: adapter(),
+    adapter: adapter()
   }
 };
 
