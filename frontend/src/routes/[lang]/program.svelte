978a147f69adfb4b09ca9486666b717773ee020c
Iver Småge men bærbar <iversmage@gmail.com>
2022-07-08 02:29:31 +0200

    fix bug

--- frontend/src/routes/[lang]/program.svelte
+++ frontend/src/routes/[lang]/program.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { browser, dev } from '$app/env';
-  import Program from '$lib/frontpage/Program.svelte';
   export const hydrate = dev;
   export const router = browser;
   export const prerender = true;
