1ac385ae6edd4db69bbaf4d487ad531b765e584e
Iver Småge men bærbar <iversmage@gmail.com>
2022-09-27 11:14:27 +0200

    update companies and program pages

--- frontend/src/routes/(user)/[lang]/companies/+page.svelte
+++ frontend/src/routes/(user)/[lang]/companies/+page.svelte
@@ -1,7 +1,6 @@
-
-
 <script lang="ts">
   import { t } from '$lib/translations/translations';
+  import companyImage from '$lib/assets/bedrifter.jpg';
 </script>
 
 <svelte:head>
@@ -11,6 +10,7 @@
 <section class="companies">
   <div class="content" id="companies">
     <h1 class="text-3xl mb-8">{$t('companies.companies')}</h1>
-    <p class="text-lg">{$t('companies.moreinfo')}</p>
+    <p class="text-lg">{$t('companies.paragraph')}</p>
+    <img src={companyImage} alt="Collage of companies participating" />
   </div>
 </section>
