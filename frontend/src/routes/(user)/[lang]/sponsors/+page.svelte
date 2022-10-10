51aace0717827c11304c4e68bc7d04dbbe7b00ff
Iver Småge men bærbar <iversmage@gmail.com>
2022-10-11 00:34:05 +0200

    update info

--- frontend/src/routes/(user)/[lang]/sponsors/+page.svelte
+++ frontend/src/routes/(user)/[lang]/sponsors/+page.svelte
@@ -1,7 +1,7 @@
-
-
 <script lang="ts">
   import { t } from '$lib/translations/translations';
+  import jotunImage from '$lib/assets/jotun.jpg';
+  import samarbeidsForumImage from '$lib/assets/samarbeidsforum.jpg';
 </script>
 
 <svelte:head>
@@ -11,7 +11,16 @@
 <section class="sponsors">
   <div class="content" id="sponsors">
     <h1 class="text-3xl mb-8">{$t('sponsors.sponsors')}</h1>
-    <p class="text-lg">{$t('sponsors.moreinfo')}</p>
-    <!-- <img src="/sponsors_22.png" alt="bedriftene som er med på kjemidagen" /> -->
+    <p class="text-lg">{$t('sponsors.intro')}</p>
+    <img src={jotunImage} alt="jotun logo" class="w-4/5 md:w-2/4" />
+    <img src={samarbeidsForumImage} alt="samarbeidsforum logo" class="w-4/5 md:w-2/4" />
+    <p class="text-lg">
+      {$t('sponsors.institutes')}
+    </p>
+    <ul class="text-lg list-disc list-inside">
+      <li>{$t('sponsors.ibt')}</li>
+      <li>{$t('sponsors.ikp')}</li>
+      <li>{$t('sponsors.ima')}</li>
+    </ul>
   </div>
 </section>
