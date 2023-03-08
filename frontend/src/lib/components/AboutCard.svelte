0af74de9bb826c0028ae7ce238d3dd0b49ec16d1
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2023-03-08 15:17:40 +0100

    Update images and board members

--- frontend/src/lib/components/AboutCard.svelte
+++ frontend/src/lib/components/AboutCard.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <article class="mb-8 mx-auto">
-  <img src={imgSrc} alt={title} class="h-96 w-80 object-cover xl:h-72 xl:w-48" />
-  <h4 class="text-lg">{name}</h4>
+  <img src={imgSrc} alt={title} class="h-96 w-80 object-cover xl:h-72 xl:w-60" />
+  <h4 class="text-lg mt-2">{name}</h4>
   <h5 class="text-md">{title}</h5>
   <h5 class="text-red text-sm xl:text-xs"><a href="mailto:{email}">{email}</a></h5>
 </article>
--- frontend/src/routes/(user)/[lang]/about/+page.svelte
+++ frontend/src/routes/(user)/[lang]/about/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import AboutCard from '$lib/components/AboutCard.svelte';
   import { t } from '$lib/translations/translations';
-  import lederSrc from '$lib/assets/styret/leder.jpg';
-  import nestlederSrc from '$lib/assets/styret/nestleder.jpg';
-  import okonomisjefSrc from '$lib/assets/styret/okonomisjef.jpg';
-  import prsjefSrc from '$lib/assets/styret/pr-sjef.jpg';
-  import riggsjefSrc from '$lib/assets/styret/rigg-sjef.jpg';
-  // import riggsjefSrc from '$lib/assets/styret/default.jpg';
+  import headSrc from '$lib/assets/styret/head.jpg';
+  import deputyHeadSrc from '$lib/assets/styret/deputy_head.jpg';
+  import cfoSrc from '$lib/assets/styret/cfo.jpg';
+  import prOfficerSrc from '$lib/assets/styret/pr_officer.jpg';
+  import chiefRiggerSrc from '$lib/assets/styret/chief_rigger.jpg';
+  import eventManagerSrc from '$lib/assets/styret/event_manager.jpg';
 </script>
 
 <svelte:head>
@@ -19,36 +19,44 @@
       <h1 class="text-3xl mb-8">{$t('common.about')}</h1>
       <p class="text-md mb-2">{$t('about.paragraph')}</p>
       <h3 class="mt-5 mb-5 text-2xl">{$t('about.theboard')}</h3>
-      <div class="m-auto gap-2 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 xl:-mx-20">
+      <div class="m-auto gap-2 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3">
         <AboutCard
-          imgSrc={lederSrc}
-          name="Pia Nedreås"
+          imgSrc={headSrc}
+          name="Emilie Sundt Hennisen"
           title={$t('about.leader')}
           email="leder@kjemidagen.no"
         />
         <AboutCard
-          imgSrc={nestlederSrc}
-          name="Celina Ryholt"
+          imgSrc={deputyHeadSrc}
+          name="Synne Sæther Lande"
           title={$t('about.deputy')}
           email="nestleder@kjemidagen.no"
         />
         <AboutCard
-          imgSrc={okonomisjefSrc}
-          name="Herman Berggren"
-          title={$t('about.economist')}
+          imgSrc={cfoSrc}
+          name="Henrik Engen Ødegård"
+          title={$t('about.cfo')}
           email="okonomiansvarlig@kjemidagen.no"
         />
+      </div>
+      <div class="m-auto gap-2 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3">
         <AboutCard
-          imgSrc={prsjefSrc}
-          name="Vivi Gunnarsrud"
-          title={$t('about.prboss')}
+          imgSrc={prOfficerSrc}
+          name="Hilde-Marie H. Gransjøen"
+          title={$t('about.pr_officer')}
           email="pr-sjef@kjemidagen.no"
         />
         <AboutCard
-          imgSrc={riggsjefSrc}
-          name="Cecilie Bakketun"
-          title={$t('about.rigg')}
+          imgSrc={chiefRiggerSrc}
+          name="Tor Gunnar R. Hammer"
+          title={$t('about.rigger')}
           email="riggeansvarlig@kjemidagen.no"
+        />
+        <AboutCard
+          imgSrc={eventManagerSrc}
+          name="Christine Laetare"
+          title={$t('about.event')}
+          email="arrangementsansvarlig@kjemidagen.no"
         />
       </div>
     </div>
