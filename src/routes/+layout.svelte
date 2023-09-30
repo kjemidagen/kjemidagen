9894b7215d48c2383ab168796bc2201866a8c321
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2023-09-30 14:21:19 +0200

    📈 Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script context="module">
   import favicon from '$lib/assets/favicon.ico';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
