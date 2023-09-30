a4b29988952b8e3fcb9d8fc4199fb4ae7cead208
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2023-09-30 18:26:16 +0200

    💄 Change mail icon in footer

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -3,11 +3,12 @@
   import linkedinSrc from '$lib/assets/linkedin.svg';
   import facebookSrc from '$lib/assets/facebook.svg';
   import instagramSrc from '$lib/assets/instagram.svg';
-  import emailSrc from '$lib/assets/email.svg';
+  import { Envelope } from '@steeze-ui/heroicons';
+  import { Icon } from '@steeze-ui/svelte-icon';
 </script>
 
-<footer class="bg-red text-gray-200 py-7 grid-area-footer px-2 z-10">
-  <div class="grid max-w-5xl m-auto grid-cols-2 gap-7 md:grid-cols-3">
+<footer class="grid-area-footer z-10 bg-red px-2 py-7 text-gray-200">
+  <div class="m-auto grid max-w-5xl grid-cols-2 gap-7 md:grid-cols-3">
     <div>
       <h6 class="text-lg">Kjemidagen</h6>
       <p class="mb-3">Org.nr 992 641 231</p>
@@ -18,20 +19,28 @@
     <div class="text-gray-200">
       <h6 class="text-lg">SoMe</h6>
       <a href="https://www.linkedin.com/company/kjemidagen/" class="text-gray-200"
-        ><img src={linkedinSrc} alt="" class="h-4 inline" /> LinkedIn</a
+        ><img src={linkedinSrc} alt="" class="inline mr-1 h-4" /> LinkedIn</a
       ><br />
       <a href="https://www.facebook.com/Kjemidagen/" class="text-gray-200"
-        ><img src={facebookSrc} alt="" class="h-4 inline" /> Facebook</a
+        ><img src={facebookSrc} alt="" class="inline mr-1 h-4" /> Facebook</a
       ><br />
       <a href="https://www.instagram.com/kjemidagen/" class="text-gray-200"
-        ><img src={instagramSrc} alt="" class="h-4 inline" /> Instagram</a
+        ><img src={instagramSrc} alt="" class="inline mr-1 h-4" /> Instagram</a
       ><br />
     </div>
     <div>
       <h6 class="text-lg">{$t('common.contactus')}</h6>
-      <a href="mailto:kontakt@kjemidagen.no" class="text-gray-200"
-        ><img src={emailSrc} alt="" class="h-4 inline" /> kontakt@kjemidagen.no</a
-      >
+      <div class="inline-flex items-center justify-center">
+        <a href="mailto:kontakt@kjemidagen.no" class="text-gray-200">
+          <Icon
+            src={Envelope}
+            size="1.2em"
+            theme="solid"
+            class="my-auto mr-1 inline leading-none text-white"
+          />
+          <span class="leading-1 text-white"> kontakt@kjemidagen.no </span>
+        </a>
+      </div>
     </div>
     <a class="text-gray-300" href="#banner">Tilbake til toppen</a>
   </div>
