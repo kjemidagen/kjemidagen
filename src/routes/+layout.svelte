ec527914da1ae7b707daa4c108b70e12f684d649
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2023-09-30 14:58:12 +0200

    📈 Fix Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script context="module">
   import favicon from '$lib/assets/favicon.ico';
-  import { dev } from '$app/environment';
-  import { inject } from '@vercel/analytics';
-
-  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,5 @@
 import '../app.css';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
