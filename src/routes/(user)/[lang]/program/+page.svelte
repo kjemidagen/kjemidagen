cbea970b6dd53cfbfb962c1e70335558dca38e58
Martin Kollstrand <martin.kollstrand@gmail.com>
2024-09-09 21:28:55 +0200

    added desc to some programs

--- src/routes/(user)/[lang]/program/+page.svelte
+++ src/routes/(user)/[lang]/program/+page.svelte
@@ -44,19 +44,31 @@
         time="11:15-12:00"
         location="{$t('program.summerjob_marathon_loc')}"
         loc_link="https://link.mazemap.com/DsnEdMUa"
-        />
+        >
+          <p class="mt-3 leading-7">
+            {$t('program.summerjob_marathon_desc')}
+          </p>
+      </Event>
       <Event
         title="{$t('program.job_marathon')}"
         time="13:15-14:00"
         location="{$t('program.job_marathon_loc')}"
         loc_link="https://link.mazemap.com/DsnEdMUa"
-        />
+        >
+          <p class="mt-3 leading-7">
+            {$t('program.job_marathon_desc')}
+          </p>
+      </Event>
       <Event
         title="{$t('program.banquet')}"
         time="18:30"
         location="{$t('program.banquet_loc')}"
         loc_link="https://www.google.no/maps/place/Frimurerlogen+Trondhjem+Selskapslokaler/@63.4300014,10.397319,17z/data=!3m1!4b1!4m6!3m5!1s0x466d319bd29d55b3:0xe0e4e3fdf8991cd1!8m2!3d63.4300014!4d10.3998939!16s%2Fg%2F1tgnvvrm?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
-        />
+        >
+          <p class="mt-3 leading-7">
+            {$t('program.banquet_desc')}
+          </p>
+      </Event>
     </div>
 
   </div>
