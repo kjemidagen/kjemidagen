8334a1f81c4c8db779e24a9f763d759fd3410bb5
Martin Kollstrand <martin.kollstrand@gmail.com>
2024-09-12 14:10:57 +0200

    Merge branch 'sponsors'

--- src/routes/(user)/[lang]/sponsors/+page.svelte
+++ src/routes/(user)/[lang]/sponsors/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { t } from '$lib/translations/translations';
-  import jotunImage from '$lib/assets/jotun.jpg';
-  import samarbeidsForumImage from '$lib/assets/samarbeidsforum.jpg';
+  import nitoImage from '$lib/assets/nitoLogo.png';
 </script>
 
 <svelte:head>
@@ -12,17 +11,16 @@
   <div class="content" id="sponsors">
     <h1 class="text-3xl mb-8">{$t('sponsors.sponsors')}</h1>
     <p class="text-lg">{$t('sponsors.intro')}</p>
-    <!--<div class="grid grid-cols-1 lg:grid-cols-2 gap-8 my-4">
-      <img src={jotunImage} alt="Jotun logo" class="w-full h-full object-contain object-center" />
-      <img src={samarbeidsForumImage} alt="Samarbeidsforum logo" class="w-full h-full object-contain object-center" />
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 my-4">
+      <img src={nitoImage} alt="Nito logo" class="w-full h-full object-contain object-center" /> 
     </div>
-  -->
     <p class="text-lg">
       {$t('sponsors.institutes')}
     </p>
     <ul class="text-lg list-disc list-inside">
       <li>{$t('sponsors.ikp')}</li>
       <li>{$t('sponsors.ima')}</li>
+      <li>{$t('sponsors.bom')}</li>      
     </ul>
   </div>
 </section>
