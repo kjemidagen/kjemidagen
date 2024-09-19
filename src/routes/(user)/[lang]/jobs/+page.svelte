da2a84f62c6b25b94e24900dc50e55908dc1ed0d
Martin Kollstrand <martin.kollstrand@gmail.com>
2024-09-19 14:52:13 +0200

    Added div for styling

--- src/routes/(user)/[lang]/jobs/+page.svelte
+++ src/routes/(user)/[lang]/jobs/+page.svelte
@@ -11,10 +11,12 @@
 
 <section class="jobs" id="jobs">
   <div class="content">
-    <h1 class="mb-8 text-3xl">{$t('jobs.title')}</h1>
-    <p>
-      {$t('jobs.description')}
-    </p>
+    <div id="description">
+      <h1 class="mb-8 text-3xl">{$t('jobs.title')}</h1>
+      <p>
+        {$t('jobs.description')}
+      </p>
+    </div>
     <div class="mt-4 grid grid-cols-1 gap-4">
       {#each jobsMap as job}
         <Advert
