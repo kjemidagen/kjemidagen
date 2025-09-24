d0889b0630addf860b96b06f8f4cf6188decb4fb
Steinar Clausen <145474895+SteinarClausen@users.noreply.github.com>
2025-09-24 22:44:23 +0200

    update program

--- src/routes/(user)/[lang]/program/+page.svelte
+++ src/routes/(user)/[lang]/program/+page.svelte
@@ -59,7 +59,7 @@
           title={$t('program.job_marathon')}
           time="13:15 - 14:00"
           location={$t('program.job_marathon_loc')}
-          loc_link=""
+          loc_link="https://link.mazemap.com/8Vx5vnjn"
         >
           <p class="mt-3 leading-7">
             {$t('program.job_marathon_desc')}
@@ -100,7 +100,7 @@
         title={$t('program.job_marathon')}
         time="13:15 - 14:00"
         location={$t('program.job_marathon_loc')}
-        loc_link="https://link.mazemap.com/YNpUWkqi"
+        loc_link="https://link.mazemap.com/8Vx5vnjn"
       >
         <p class="mt-3 leading-7">
           {$t('program.job_marathon_desc')}
@@ -110,7 +110,7 @@
         title={$t('program.banquet')}
         time="18:30"
         location={$t('program.banquet_loc')}
-        loc_link=""
+        loc_link="google.com/maps/place//data=!4m2!3m1!1s0x466d319bd29d55b3:0xe0e4e3fdf8991cd1?sa=X&ved=1t:8290&ictx=111"
       >
         <p class="mt-3 leading-7">
           {$t('program.banquet_desc')}
