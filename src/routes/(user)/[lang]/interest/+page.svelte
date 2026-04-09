7516181ef7a94b602a68a4a94899584b5377dd35
Steinar Clausen <145474895+SteinarClausen@users.noreply.github.com>
2026-04-09 17:47:54 +0200

    do better mainclass padding handling

--- src/routes/(user)/[lang]/interest/+page.svelte
+++ src/routes/(user)/[lang]/interest/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { t, locale } from '$lib/translations/translations';
+  import { onMount, onDestroy } from "svelte";
+  import { t } from '$lib/translations/translations';
   
-  $: other_lang = $locale === "en" ? "no" : "en";
   let formUrl = "https://forms.office.com/Pages/ResponsePage.aspx?id=cgahCS-CZ0SluluzdZZ8BWRw7RWtPeFLoYPXAOQ4SHJUREpPT0NSMkNIN05ZNVNOUkhHUklaVVdWOC4u"
   onMount(() => {
     const mainElement = document.getElementById('main');
     if (mainElement) {
-      mainElement.classList.add('px-0');
+      mainElement.classList.remove('px-2');
+    }
+  });
+  onDestroy(() => {
+    const mainElement = document.getElementById('main');
+    if (mainElement) {
+      mainElement.classList.add('px-2');
     }
   });
 </script>
@@ -20,7 +25,7 @@
   <iframe 
     title="interest-form"
     src={formUrl} 
-    class="flex-grow w-full"
+    class="flex-grow h-full w-full"
     frameborder="0" 
     allowfullscreen>
   </iframe>
